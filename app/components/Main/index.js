39157d20f1a6dc671744c269b521847221ecd2f1
Marcel Bührig <marcel.buehrig@zeix.com>
2017-02-08 19:27:33 +0100

    Updated main component

--- app/components/Main/index.js
+++ app/components/Main/index.js
@@ -6,7 +6,7 @@ class Main extends React.Component {
     return (
         <div>
           <h1>
-            <Link to="/">Mäsi is hot!</Link>
+            <Link to="/">Zeix is hot!</Link>
           </h1>
           {React.cloneElement(this.props.children, this.props)}
         </div>
