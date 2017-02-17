f85c9b716b6b5d084e37a275151ec8737a85492a
Marcel Bührig <marcel.buehrig@zeix.com>
2017-02-17 14:17:22 +0100

    Added create-zeix-app bin

--- bin/create-zeix-app.js
+++ bin/create-zeix-app.js
@@ -1,3 +1,22 @@
-var greeting = "Hello"
+#!/usr/bin/env node
 
-console.log(greeting);
+const path = require('path');
+const ncp = require('ncp').ncp;
+const nodePath = process.argv[0];
+const newFolder = process.argv[2];
+const pathBoilerplate = path.join(nodePath, '../..', 'lib/node_modules/zeix-react-redux-boilerplate');
+const newProjectPath = path.join(process.env.PWD, newFolder);
+
+if (typeof newFolder === typeof undefined) {
+  console.error('After "create-zeix-app" you have to enter the name of the project so we can create the folder');
+
+  return false;
+}
+
+ncp(pathBoilerplate, newProjectPath, {dereference: true}, function (err) {
+  if (err) {
+    return console.error(err);
+  }
+
+  console.log('We created your app! Hurray');
+})
